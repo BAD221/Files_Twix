d4ab0b0f0af75036ec969ccf6ece66074ea57d28
HIEDAR ZEAD <51862485+BAD221@users.noreply.github.com>
2021-11-21 21:06:03 +0300

    Update ChangePhoto.lua

--- File_Bot/ChangePhoto.lua
+++ File_Bot/ChangePhoto.lua
@@ -4,25 +4,25 @@ if ChatType == 'sp' or ChatType == 'gp'  then
 if text then 
 tdcli_function({ID = "GetUser",user_id_ = msg.sender_user_id_},function(arg,result)
 if result.id_ then 
-local hid = Devhid:get("Twix:Photo"..result.id_)
+local Hid = DevHid:get("Twix:Photo"..result.id_)
 if not result.profile_photo_ then 
-if hid then 
-Dev_hid(msg.chat_id_, msg.id_, 1, "حذف كل صوره مضروب بوري، 😹💔", 1, 'html')
-Devhid:del("Twix:Photo"..result.id_) 
+if Hid then 
+Dev_Hid(msg.chat_id_, msg.id_, 1, "حذف كل صوره مضروب بوري، 😹💔", 1, 'html')
+DevHid:del("Twix:Photo"..result.id_) 
 end
 end
 if result.profile_photo_ then 
-if hid and hid ~= result.profile_photo_.big_.persistent_id_ then 
-local hid_text = {
+if Hid and Hid ~= result.profile_photo_.big_.persistent_id_ then 
+local Hid_text = {
 "وفف مو صوره غنبلةة، 🤤♥️",
 "طالع صاكك بالصوره الجديده ممكن نرتبط؟ ، 🤤♥️",
 "حطيت صوره جديده عود شوفوني اني صاكك بنات، 😹♥️",
 "اححح شنيي هالصوره الجديده، 🤤♥️",
 }
-hid3 = math.random(#hid_text)
-Dev_hid(msg.chat_id_, msg.id_, 1, hid_text[hid3], 1, 'html')
+Hid3 = math.random(#hid_text)
+Dev_Hid(msg.chat_id_, msg.id_, 1, hid_text[Hid3], 1, 'html')
 end  
-Devhid:set("Twix:Photo"..result.id_, result.profile_photo_.big_.persistent_id_) 
+DevHid:set("Twix:Photo"..result.id_, result.profile_photo_.big_.persistent_id_) 
 end
 end
 end,nil) 
