4d1aebd7044c68e20f148be957e8b148755aa98e
HIEDAR ZEAD <51862485+BAD221@users.noreply.github.com>
2021-11-21 21:08:32 +0300

    Update ProNames.lua

--- File_Bot/ProNames.lua
+++ File_Bot/ProNames.lua
@@ -3,10 +3,10 @@ local text = msg.content_.text_
 if ChatType == 'sp' or ChatType == 'gp'  then
 if text and (text:match("^وضع توحيد (.*)$") or text:match("^ضع توحيد (.*)$")) then
 if Manager(msg) then
-if Devhid:get(Twix.."hid:Lock:ProNames"..msg.chat_id_) then
+if DevHid:get(Twix.."Hid:Lock:ProNames"..msg.chat_id_) then
 local Txt = text:match("^وضع توحيد (.*)$") or text:match("^ضع توحيد (.*)$")
 send(msg.chat_id_, msg.id_,'⌔︙تم تعيين ↫ '..Txt..' كتوحيد للمجموعه')
-Devhid:set(Twix.."hid:ProNames:Txt"..msg.chat_id_,Txt)
+DevHid:set(Twix.."Hid:ProNames:Txt"..msg.chat_id_,Txt)
 else
 send(msg.chat_id_, msg.id_,'⌔︙ميزة التوحيد معطله يرجى تفعيلها')
 end
@@ -14,20 +14,20 @@ end
 end
 if text and (text:match("^تعين عدد الكتم (.*)$") or text:match("^تعيين عدد الكتم (.*)$")) then
 if Manager(msg) then
-if Devhid:get(Twix.."hid:Lock:ProNames"..msg.chat_id_) then
+if DevHid:get(Twix.."Hid:Lock:ProNames"..msg.chat_id_) then
 local Num = text:match("^تعين عدد الكتم (.*)$") or text:match("^تعيين عدد الكتم (.*)$")
 send(msg.chat_id_, msg.id_,'⌔︙تم تعيين  ↫ '..Num..' عدد الكتم')
-Devhid:set(Twix.."hid:ProNames:Num"..msg.chat_id_,Num)
+DevHid:set(Twix.."Hid:ProNames:Num"..msg.chat_id_,Num)
 else
 send(msg.chat_id_, msg.id_,'⌔︙ميزة التوحيد معطله يرجى تفعيلها')
 end
 end
 end
-if Devhid:get(Twix.."hid:Lock:ProNames"..msg.chat_id_) then
+if DevHid:get(Twix.."Hid:Lock:ProNames"..msg.chat_id_) then
 if text == "التوحيد" or text == "توحيد" then
-if Devhid:get(Twix.."hid:ProNames:Txt"..msg.chat_id_) then
-local ProNamesTxt = Devhid:get(Twix.."hid:ProNames:Txt"..msg.chat_id_)
-local ProNamesNum = Devhid:get(Twix.."hid:ProNames:Num"..msg.chat_id_) or 5
+if DevHid:get(Twix.."Hid:ProNames:Txt"..msg.chat_id_) then
+local ProNamesTxt = DevHid:get(Twix.."Hid:ProNames:Txt"..msg.chat_id_)
+local ProNamesNum = DevHid:get(Twix.."Hid:ProNames:Num"..msg.chat_id_) or 5
 send(msg.chat_id_, msg.id_,'⌔︙التوحيد هو ↫ '..ProNamesTxt..'\n⌔︙عدد المحاولات للكتم ↫ '..ProNamesNum)
 else
 send(msg.chat_id_, msg.id_,'⌔︙لم يتم تعيين توحيد للمجموعه')
@@ -35,21 +35,21 @@ end
 end
 end
 if not msg.forward_info_ and not Constructor(msg) then
-if Devhid:get(Twix.."hid:Lock:ProNames"..msg.chat_id_) and Devhid:get(Twix.."hid:ProNames:Txt"..msg.chat_id_) then 
+if DevHid:get(Twix.."Hid:Lock:ProNames"..msg.chat_id_) and DevHid:get(Twix.."Hid:ProNames:Txt"..msg.chat_id_) then 
 tdcli_function ({ID = "GetUser",user_id_ = msg.sender_user_id_},function(extra,result,success)
 if result and result.first_name_ then 
-if result.first_name_:match("(.*)"..Devhid:get(Twix.."hid:ProNames:Txt"..msg.chat_id_).."(.*)") then 
-Devhid:srem(Twix..'hid:Muted:'..msg.chat_id_, msg.sender_user_id_)
+if result.first_name_:match("(.*)"..DevHid:get(Twix.."Hid:ProNames:Txt"..msg.chat_id_).."(.*)") then 
+DevHid:srem(Twix..'Hid:Muted:'..msg.chat_id_, msg.sender_user_id_)
 else
-local ProNamesTxt = Devhid:get(Twix.."hid:ProNames:Num"..msg.chat_id_) or 5
-local UserNum = Devhid:get(Twix.."hid:ProNames:UserNum"..msg.chat_id_..msg.sender_user_id_) or 0
+local ProNamesTxt = DevHid:get(Twix.."Hid:ProNames:Num"..msg.chat_id_) or 5
+local UserNum = DevHid:get(Twix.."Hid:ProNames:UserNum"..msg.chat_id_..msg.sender_user_id_) or 0
 if (tonumber(UserNum) == tonumber(ProNamesTxt) or tonumber(UserNum) > tonumber(ProNamesTxt)) then 
-Devhid:sadd(Twix..'hid:Muted:'..msg.chat_id_, msg.sender_user_id_)
-Devhid:del(Twix.."hid:ProNames:UserNum"..msg.chat_id_..msg.sender_user_id_)
+DevHid:sadd(Twix..'Hid:Muted:'..msg.chat_id_, msg.sender_user_id_)
+DevHid:del(Twix.."Hid:ProNames:UserNum"..msg.chat_id_..msg.sender_user_id_)
 send(msg.chat_id_, msg.id_,"⌔︙العضو ↫ ["..result.first_name_.."](https://t.me/"..(result.username_ or "Dev_Prox")..")\n⌔︙تم كتمه بسبب عدم وضع توحيد المجموعه بجانب اسمه")
 else 
-Devhid:incrby(Twix.."hid:ProNames:UserNum"..msg.chat_id_..msg.sender_user_id_,1)
-send(msg.chat_id_, msg.id_, "⌔︙عذرا عزيزي ↫ ["..result.first_name_.."](https://t.me/"..(result.username_ or "Dev_Prox")..")\n⌔︙عليك وضع التوحيد ↫ `"..Devhid:get(Twix.."hid:ProNames:Txt"..msg.chat_id_).."` بجانب اسمك\n⌔︙عدد المحاولات المتبقيه ↫ "..(tonumber(ProNamesTxt) - tonumber(UserNum)).."")
+DevHid:incrby(Twix.."Hid:ProNames:UserNum"..msg.chat_id_..msg.sender_user_id_,1)
+send(msg.chat_id_, msg.id_, "⌔︙عذرا عزيزي ↫ ["..result.first_name_.."](https://t.me/"..(result.username_ or "Dev_Prox")..")\n⌔︙عليك وضع التوحيد ↫ `"..DevHid:get(Twix.."Hid:ProNames:Txt"..msg.chat_id_).."` بجانب اسمك\n⌔︙عدد المحاولات المتبقيه ↫ "..(tonumber(ProNamesTxt) - tonumber(UserNum)).."")
 end
 end
 end
@@ -59,11 +59,11 @@ end
 
 if text == "تفعيل التوحيد" and Constructor(msg) then
 send(msg.chat_id_, msg.id_, '⌔︙تم تفعيل توحيد المجموعه')
-Devhid:set(Twix.."hid:Lock:ProNames"..msg.chat_id_,true)
+DevHid:set(Twix.."Hid:Lock:ProNames"..msg.chat_id_,true)
 end
 if text == "تعطيل التوحيد" and Constructor(msg) then
 send(msg.chat_id_, msg.id_, '⌔︙تم تعطيل توحيد المجموعه')
-Devhid:del(Twix.."hid:Lock:ProNames"..msg.chat_id_)
+DevHid:del(Twix.."Hid:Lock:ProNames"..msg.chat_id_)
 end
 end
 
