3be0aadaa3de97ad767f2cca858b4ba63cf7a6aa
HIEDAR ZEAD <51862485+BAD221@users.noreply.github.com>
2021-11-21 21:06:52 +0300

    Update ChangeUser.lua

--- File_Bot/ChangeUser.lua
+++ File_Bot/ChangeUser.lua
@@ -4,25 +4,25 @@ if ChatType == 'sp' or ChatType == 'gp'  then
 if text then 
 tdcli_function({ID = "GetUser",user_id_ = msg.sender_user_id_},function(arg,result)
 if result.id_ then 
-local hid = Devhid:get("Twix:User"..result.id_)
+local Hid = DevHid:get("Twix:User"..result.id_)
 if not result.username_ then 
-if hid then 
-Dev_hid(msg.chat_id_, msg.id_, 1, "حذف معرفه خمطو بسرعه، 😹💔 \nهذا معرفه @"..hid.."", 1, 'html')
-Devhid:del("Twix:User"..result.id_) 
+if Hid then 
+Dev_Hid(msg.chat_id_, msg.id_, 1, "حذف معرفه خمطو بسرعه، 😹💔 \nهذا معرفه @"..Hid.."", 1, 'html')
+DevHid:del("Twix:User"..result.id_) 
 end
 end
 if result.username_ then 
-if hid and hid ~= result.username_ then 
-local hid_text = {
+if Hid and Hid ~= result.username_ then 
+local Hid_text = {
 'معرفك الجديد عشره بربع محد ياخذه😹💔',
 "هاها غيرت معرفك نشروك بقناة فضايح😹💔💭",
 "معرفك الجديد حلو منين خامطه؟!🤤♥️",
 "معرفك القديم @"..result.username_.." ضمه بقناة لاينبعص، 😹♥️",
 }
-hids = math.random(#hid_text)
-Dev_hid(msg.chat_id_, msg.id_, 1, hid_text[hids], 1, 'html')
+Hids = math.random(#Hid_text)
+Dev_Hid(msg.chat_id_, msg.id_, 1, Hid_text[Hids], 1, 'html')
 end  
-Devhid:set("Twix:User"..result.id_, result.username_) 
+DevHid:set("Twix:User"..result.id_, result.username_) 
 end
 end
 end,nil) 
